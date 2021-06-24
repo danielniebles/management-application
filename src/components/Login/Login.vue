5f6c9332116b618e930547d520ba790874046df1
María Camila Niebles Reyes <cnieblesr@MacBook-Pro-de-Maria.local>
2021-06-24 16:00:48 -0500

    First button version

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <button v-google-signin-button='clientId' class="google-signin-button">Continue with google</button>
+</template>
+<script>
+import GoogleSignInButton from 'vue-google-signin-button-directive'
+
+export default {
+  directives: {
+    GoogleSignInButton
+  },
+  data: () => ({
+    clientId: '674205569173-a3th7fuh9mueb0rmjdod509al8fgl29o.apps.googleusercontent.com'
+  }),
+  methods: {
+    OnGoogleAuthSuccess (idToken) {
+      // Receive the idToken and make your magic with the backend
+    },
+    OnGoogleAuthFail (error) {
+      console.log(error)
+    }
+  }
+}
+
+</script>
+<style>
+
+</style>
