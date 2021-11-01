94ef7c62ca30fad3e2e7071e7a7e77903532feaa
danielniebles <danielf.niebles@gmail.com>
2021-11-01 11:51:40 -0500

    Add global styles for buttons and inputs

--- src/shared/StyleConstants.ts
+++ src/shared/StyleConstants.ts
@@ -1,10 +1,21 @@
 export const STYLE_CLASSES = {
-  PRIMARY_BTN: "btn-primary rounded-pill white--text",
+  PRIMARY_BTN: "btn-primary white--text",
   SECONDARY_BTN: "btn-secondary rounded-pill white--text",
-  PRIMARY_BTN_COMMON: "btn-primary white--text"
+  PRIMARY_BTN_COMMON: "btn-primary white--text",
+  OUTLINED_BTN: "btn-outlined"
+}
+
+export const TEXT_CLASSES = {
+  COLORED_SUBHEADER_HEAVY: "text-subtitle-1 font-weight-bold candidate__header",
+  SUBHEADER_HEAVY: "text-subtitle-1 font-weight-bold",
+  BODY_LIGHT: "text-body-2"
 }
 
 export const COLORS = {
   PRIMARY_COLOR: "#FF5A34",
-  SECONDARY_COLOR: "rgba(0, 0, 0, 0.6)"
+  SECONDARY_COLOR: "rgba(0, 0, 0, 0.6)",
+  SUCCESS: "#3CC13B",
+  WARNING: "#F3BB1C",
+  ERROR: "#F03738",
+  INFO: "#000000"
 }
