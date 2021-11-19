0496a39b212105938afab3dc30eead777a77248c
danielniebles <danielf.niebles@gmail.com>
2021-11-18 20:32:10 -0500

    Add outlook button style

--- src/modules/Login/components/OutlookButton.vue
+++ src/modules/Login/components/OutlookButton.vue
@@ -1,12 +1,12 @@
 <template>
-  <div :class="classType">
+  <div class="outlook-btn">
     <div class="google-icon-wrapper">
       <img
         class="google-icon"
-        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+        src="https://upload.wikimedia.org/wikipedia/commons/9/90/Outlook.com_icon_%282012-2019%29.svg"
       />
     </div>
-    <button @click="outlookEvent">sefaseses</button>
+    <button class="btn-text" @click="outlookEvent"><b>Inicia sesión con Outlook</b></button>
   </div>
 </template>
 
@@ -24,27 +24,6 @@ export default class OutlookButton extends Vue {
     outlookEvent(){
         this.$emit('loginOutlook');
     }
-//   GoogleSignInButton = require('vue-google-signin-button-directive')
-  
-//   @Prop() private actionType!: string;
-//   classType =
-//     this.actionType === "login" ? "google-btn" : "google-btn-register";
-//   displayText =
-//     this.actionType === "login"
-//       ? "Inicia sesión con Outlook"
-//       : "Regístrate con Outlook";
-
-//   clientId =
-//     "674205569173-a3th7fuh9mueb0rmjdod509al8fgl29o.apps.googleusercontent.com";
-
-//   OnGoogleAuthSuccess(idToken: string) {
-//     console.log(this.actionType)
-//     this.$emit(this.actionType, idToken);
-//   }
-//   OnGoogleAuthFail(error: string) {
-//     Snackbar.popError('Error de autenticación de Google');
-//   }
-
 }
 </script>
 
@@ -54,8 +33,8 @@ $button-active-white: rgb(187, 211, 211);
 $google-blue: #4285f4;
 $button-active-blue: #1669f2;
 
-.google-btn {
-  width: 215px;
+.outlook-btn {
+  width: 240px;
   height: 42px;
   background-color: $google-blue;
   border-radius: 2px;
@@ -91,41 +70,5 @@ $button-active-blue: #1669f2;
   }
 }
 
-.google-btn-register {
-  width: 200px;
-  height: 42px;
-  background-color: $white;
-  border-radius: 2px;
-  box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.25);
-  border: 1px $google-blue;
-  .google-icon-wrapper {
-    position: absolute;
-    margin-top: 1px;
-    margin-left: 1px;
-    width: 40px;
-    height: 40px;
-    border-radius: 2px;
-    background-color: $white;
-  }
-  .google-icon {
-    position: absolute;
-    margin-top: 11px;
-    margin-left: 11px;
-    width: 18px;
-    height: 18px;
-  }
-  .btn-text {
-    float: right;
-    margin: 11px 11px 0 0;
-    color: $google-blue;
-    font-size: 14px;
-    letter-spacing: 0.2px;
-  }
-  &:hover {
-    box-shadow: 0 0 6px $google-blue;
-  }
-  &:active {
-    background: $button-active-white;
-  }
-}
+
 </style>
